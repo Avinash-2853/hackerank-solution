01126c3a2dc27329b238fd67fdb2757ac3369a90
Avinash <avinash.prajapati2853@gmail.com>
2025-12-31 19:02:41 +0530

    feat: update greet function for formal greeting

--- app.py
+++ app.py
@@ -8,8 +8,4 @@ def add_to_cart(item):
     print(f"{item} added")
 
 def greet(name):
-<<<<<<< HEAD
-    print(f"Hello, {name}. Welcome.")
-=======
     print(f"Hi {name}")
->>>>>>> 26d38d0 (feat: formal greeting)
